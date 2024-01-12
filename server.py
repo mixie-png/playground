df417db19f769a06c4c07dba9e8ae1f2f0fc7f07
Mixie Clerard <clerardm@gmail.com>
2024-01-11 22:44:59 -0500

    first commit

---
+++ server.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template  # added render_template!
+app = Flask(__name__)
+
+@app.route('/play')
+def play():
+    x=3
+    return render_template('index.html', x = x) # returns the result of the render_template method, passing in the name of our HTML file
+
+@app.route('/play/<int:x>')
+def play_box(x):
+    return render_template('index.html', x = x)
+
+@app.route('/play/<int:x>/<string:color>')
+def play_boxes(x, color):
+    return render_template('index.html', x = x, color = color)
+
+
+if __name__=="__main__":
+    app.run(debug=True)
