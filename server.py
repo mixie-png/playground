4042318a68ef19c79266f2ecf9d7f295131548f4
Mixie Clerard <clerardm@gmail.com>
2024-01-12 02:23:17 -0500

    update play function

--- server.py
+++ server.py
@@ -2,8 +2,7 @@ from flask import Flask, render_template  # added render_template!
 app = Flask(__name__)
 
 @app.route('/play')
-def play():
-    x=3
+def play(x=3):
     return render_template('index.html', x = x) # returns the result of the render_template method, passing in the name of our HTML file
 
 @app.route('/play/<int:x>')
